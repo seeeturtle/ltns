e52f083ea71238f6cc97260cfe36e5b967f8d77c
joshua1b <joshua1b2823@gmail.com>
2018-12-23 21:18:15 +0900

    parser: add position information to tag
    it is not used yet, but it will be added to instances
    directly later.
    minor fix:
    * put token.value instead of token itself
    * remove unnecessary comment
    * use explicit relative import

--- ltns/parser.py
+++ ltns/parser.py
@@ -2,8 +2,8 @@ from collections import namedtuple
 
 from rply import ParserGenerator
 
-from lexer import lexer
-from models import (
+from .lexer import lexer
+from .models import (
     LtnsElement,
     LtnsKeyword,
     LtnsSymbol,
@@ -16,7 +16,7 @@ from models import (
 
 pg = ParserGenerator([rule.name for rule in lexer.rules] + ['end$'])
 
-tag = namedtuple('tag', ('name', 'attributes', 'token'))
+tag = namedtuple('tag', ('name', 'attributes', 'lineno', 'colno'))
 
 @pg.production("main : list_contents")
 def main(p):
@@ -42,20 +42,20 @@ def term(p):
 
 @pg.production("element : start_tag list_contents end_tag")
 def element(p):
-    return LtnsElement(p[0].name, p[0].token, attributes=p[0].attributes, childs=p[1])
+    return LtnsElement(p[0].name, attributes=p[0].attributes, childs=p[1])
 
 @pg.production("element : start_tag end_tag")
 @pg.production("element : empty_tag")
 def empty_element(p):
-    return LtnsElement(p[0].name, p[0].token, attributes=p[0].attributes)
+    return LtnsElement(p[0].name, attributes=p[0].attributes)
 
 @pg.production("start_tag : LANGLE identifier attributes RANGLE")
 def start_tag(p):
-    return tag(p[1].name, p[2], p[0])
+    return tag(p[1].name, p[2], p[0].source_pos.lineno, p[0].source_pos.colno)
 
 @pg.production("start_tag : LANGLE identifier RANGLE")
 def start_tag_without_attributes(p):
-    return tag(p[1].name, {}, p[0])
+    return tag(p[1].name, {}, p[0].source_pos.lineno, p[0].source_pos.colno)
 
 @pg.production("attributes : identifier EQUAL term attributes")
 def attributes(p):
@@ -70,15 +70,15 @@ def attributes_one(p):
 
 @pg.production("end_tag : LSLASHANGLE identifier RANGLE")
 def end_tag(p):
-    return tag(p[1].name, None, None)
+    return tag(p[1].name, None, None, None)
 
 @pg.production("empty_tag : LANGLE identifier attributes RSLASHANGLE")
 def empty_tag(p):
-    return tag(p[1].name, p[2], p[0])
+    return tag(p[1].name, p[2], p[0].source_pos.lineno, p[0].source_pos.colno)
 
 @pg.production("identifier : IDENTIFIER")
 def identifier(p):
-    s = p[0].getstr()
+    s = p[0].value
 
     # get base of integer
     prefix_base = {
@@ -92,47 +92,36 @@ def identifier(p):
         if s.startswith(prefix):
             base = _base
     try:
-        return LtnsInteger(p[0], base=base)
+        return LtnsInteger(s, base=base)
     except ValueError:
         pass
 
     try:
-        return LtnsFloat(p[0])
+        return LtnsFloat(s)
     except ValueError:
         pass
 
     try:
-        return LtnsComplex(p[0])
+        return LtnsComplex(s)
     except ValueError:
         pass
 
     try:
-        return LtnsKeyword(p[0])
+        if not s.startswith(':'):
+            raise ValueError
+        s = s[1:]
+        return LtnsKeyword(s)
     except ValueError:
         pass
 
-    return LtnsSymbol(p[0])
+    return LtnsSymbol(s)
 
 @pg.production("string : STRING")
 def string(p):
-    return LtnsString(p[0])
+    return LtnsString(p.value[1:-1])
 
 @pg.error
 def error_handler(token):
     raise ValueError("Ran into a %s where it wasn't expected" % token.gettokentype())
 
 parser = pg.build()
-
-# test = '''
-# <let
-#   square = <->2 1</->
-#   y = <+>1 2</+>
-# />
-
-# <square>y</square> <!-- result: 9 -->
-# '''
-
-# result = parser.parse(lexer.lex(test))
-
-# for e in result:
-#     pprint(e.__dict__)
