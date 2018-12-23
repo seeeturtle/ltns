70df8ab6476dda39738aae8c095f6e6266334b57
joshua1b <joshua1b2823@gmail.com>
2018-12-23 21:11:41 +0900

    models: remove LtnsObject and fix constructor
    * adding position propertes directly to instance is more
    flexible than making base class to define common attribute.
    defining common attributes in base class just make tasks to
    fix init, new method of class
    * constructor will not receive token but str or other types.
    the position information is optional so that you can add it
    later. this will make testing much easier.

--- ltns/models.py
+++ ltns/models.py
@@ -1,38 +1,27 @@
 from functools import reduce
 
 
-class LtnsObject:
-    def __init__(self, token, **kwargs):
-        self.lineno = token.getsourcepos().lineno - 1
-        self.col_offset = token.getsourcepos().colno - 1
-
-class LtnsElement(LtnsObject):
-    def __init__(self, name, start_token, **kwargs):
+class LtnsElement:
+    def __init__(self, name, **kwargs):
         self.name = name
         self.attributes = kwargs.pop('attributes', {})
         self.childs = kwargs.pop('childs', [])
 
-        super().__init__(start_token, **kwargs)
-
-class LtnsInteger(LtnsObject, int):
-    def __new__(cls, token, **kwargs):
-        return super().__new__(cls, token.getstr(), **kwargs)
+class LtnsInteger(int):
+    def __new__(cls, n, **kwargs):
+        return super().__new__(cls, n, **kwargs)
 
 class LtnsFloat(float):
-    def __new__(cls, token, **kwargs):
-        return super().__new__(cls, token.getstr(), **kwargs)
+    def __new__(cls, n, **kwargs):
+        return super().__new__(cls, n, **kwargs)
 
-class LtnsComplex(LtnsObject, complex):
-    def __new__(cls, token, **kwargs):
-        return super().__new__(cls, token.getstr(), **kwargs)
+class LtnsComplex(complex):
+    def __new__(cls, n, **kwargs):
+        return super().__new__(cls, n, **kwargs)
 
-class LtnsKeyword(LtnsObject):
-    def __init__(self, token, **kwargs):
-        if not token.getstr().startswith(':'):
-            raise ValueError
-        self.value = token.getstr()
-
-        super().__init__(token, **kwargs)
+class LtnsKeyword:
+    def __init__(self, name, **kwargs):
+        self.value = name
 
     def __eq__(self, other):
         return self.value == other.value
@@ -42,15 +31,10 @@ class LtnsKeyword(LtnsObject):
             lambda x, y: 1/2*(x+y)*(x+y+1)+y,
             (ord(c) for c in self.value))
 
-class LtnsString(LtnsObject, str):
-    def __new__(cls, token, **kwargs):
-        return super().__new__(cls, token.getstr()[1:-1], **kwargs)
+class LtnsString(str):
+    def __new__(cls, s, **kwargs):
+        return super().__new__(cls, s, **kwargs)
 
-class LtnsSymbol(LtnsObject):
-    def __init__(self, token, **kwargs):
-        self.name = token.getstr()
-
-        super().__init__(token, **kwargs)
-
-    def __hash__(self):
-        return self.name
+class LtnsSymbol:
+    def __init__(self, name, **kwargs):
+        self.name = name
