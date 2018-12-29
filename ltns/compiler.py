fbb0c7731aaea367066b8a96547adbac393b923d
joshua1b <joshua1b2823@gmail.com>
2018-12-29 15:41:39 +0900

    support special forms
    * `do`
    * `if`
    * binary operations

--- ltns/compiler.py
+++ ltns/compiler.py
@@ -56,12 +56,42 @@ def model(node_type):
         return f
     return decorator
 
+_special_form_compiler = {}
+
+def special(name, args=['name', 'childs', 'attrs']):
+    def decorator(f):
+        _special_form_compiler[name] = (f, args)
+        return f
+    return decorator
+
 class LtnsCompiler:
+    _temp = 0
+
+    def _temp_func_name(self):
+        self._temp += 1
+        return f'_temp_func_{self._temp}'
+
+    def _temp_var_name(self):
+        self._temp += 1
+        return f'_temp_var_{self._temp}'
+
     def compile(self, node):
         return _model_compiler[type(node)](self, node)
 
     @model(LtnsElement)
     def compile_element(self, element):
+        if element.name in _special_form_compiler:
+            sf, args = _special_form_compiler[element.name]
+
+            data = {
+                'name': element.name,
+                'childs': element.childs,
+                'attrs': element.attributes,
+            }
+            args = [data[arg] for arg in args]
+
+            return sf(self, *args)
+
         func = ast.parse(element.name, mode='eval').body # TODO: handle exception of parsing
 
         result = Result()
@@ -129,3 +159,122 @@ class LtnsCompiler:
         result.expr = expr
 
         return result
+
+    @special('do', ['childs'])
+    def compile_do(self, childs):
+        result = Result()
+
+        childs = [self.compile(child) for child in childs]
+
+        for child in childs[:-1]:
+            result.stmts += child.stmts
+            result.stmts.append(child.expr_statement)
+
+        expr = childs[-1].expr
+        result.stmts += childs[-1].stmts
+        result.expr = expr
+
+        return result
+
+    name_op = {
+        'add*': ast.Add(),
+        'sub*': ast.Sub(),
+        'mul*': ast.Mult(),
+        'div*': ast.Div(),
+        'mod': ast.Mod(),
+        'pow': ast.Pow(),
+        'lshift': ast.LShift(),
+        'rshift': ast.RShift(),
+        'bitor': ast.BitOr(),
+        'bitxor': ast.BitXor(),
+        'bitand': ast.BitAnd(),
+    }
+
+    def compile_bin_op(self, name, childs):
+        result = Result()
+
+        left = self.compile(childs[0])
+        result.stmts += left.stmts
+        left = left.expr
+
+        right = self.compile(childs[1])
+        result.stmts += right.stmts
+        right = right.expr
+
+        expr = ast.BinOp(op=self.name_op[name], left=left, right=right)
+        result.expr = expr
+
+        return result
+
+    for name in name_op:
+        _special_form_compiler[name] = (compile_bin_op, ['name', 'childs'])
+
+    @special('if', ['childs'])
+    def compile_if(self, childs):
+        result = Result()
+
+        pred = self.compile(childs[0])
+        result.stmts += pred.stmts
+
+        then_body = self.compile(childs[1])
+
+        try:
+            else_body = self.compile(childs[2])
+        except IndexError:
+            else_body = None
+
+        if then_body.stmts or else_body.stmts:
+            temp_func_name = self._temp_func_name()
+            temp_var_name = self._temp_var_name()
+
+            body = then_body.stmts
+            body.append(
+                ast.Assign(
+                    targets=[ast.Name(id=temp_var_name, ctx=ast.Store())],
+                    value=then_body.expr,
+                )
+            )
+
+            orelse = else_body.stmts
+            orelse.append(
+                ast.Assign(
+                    targets=[ast.Name(id=temp_var_name, ctx=ast.Store())],
+                    value=else_body.expr,
+                )
+            )
+
+            result.stmts.append(
+                ast.FunctionDef(
+                    name=temp_func_name,
+                    args=ast.arguments(
+                        args=[],
+                        vararg=None,
+                        kwonlyargs=[],
+                        kw_defaults=[],
+                        kwarg=None,
+                        defaults=[],
+                    ),
+                    body=[
+                        ast.If(test=pred.expr, body=body, orelse=orelse),
+                        ast.Return(value=ast.Name(id=temp_var_name, ctx=ast.Load())),
+                    ],
+                    decorator_list=[],
+                    returns=None,
+                )
+            )
+
+            result.expr = ast.Call(
+                func=ast.Name(id=temp_func_name, ctx=ast.Load()),
+                args=[],
+                keywords=[],
+            )
+
+            return result
+        else:
+            result.expr = ast.IfExp(
+                test=pred.expr,
+                body=then_body.expr,
+                orelse=else_body.expr,
+            )
+
+            return result
